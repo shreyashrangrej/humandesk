d18a60c9c453dcbefd966b1a7736cb4e6f83bdec
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-10-27 09:50:51 +0530

    installed tailwind

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-3xl font-bold underline">
           Welcome to HumanDesk
         </h1>
       </main>
