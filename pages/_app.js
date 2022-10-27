b9dfbcf148d3bc3acfd6b36517ac43e33bcf1f3b
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-10-27 14:56:44 +0530

    How to set auth cookies

--- pages/_app.js
+++ pages/_app.js
@@ -2,79 +2,16 @@ import '../styles/globals.css'
 import { useState, useEffect } from 'react'
 import { supabase } from '../util/supabaseClient'
 import { useRouter } from 'next/router'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 function MyApp({ Component, pageProps }) {
-  const [authenticatedState, setAuthenticatedState] = useState('not-authenticated')
   const router = useRouter()
-
-  useEffect(() => {
-    debugger;
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      debugger;
-      handleAuthChange(event, session)
-      if (event === 'SIGNED_IN') {
-        setAuthenticatedState('authenticated')
-        router.push('/profile')
-      }
-      if (event === 'SIGNED_OUT') {
-        setAuthenticatedState('not-authenticated')
-      }
-    })
-    checkUser()
-    return () => {
-      authListener.subscription.unsubscribe()
-    }
-  }, [])
-
-  async function handleAuthChange(event, session) {
-    await fetch('/api/auth', {
-      method: 'POST',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      credentials: 'same-origin',
-      body: JSON.stringify({ event, session })
-    })
-  }
-
-  async function checkUser() {
-    const user = await supabase.auth.getUser()
-
-    if(user) {
-      setAuthenticatedState('authenticated')
-    }
-  }
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
   return (
-    
-    <div>
-      {/* <nav style={navStyle}>
-        <Link href='/'>
-          <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href='/profile'>
-          <a style={linkStyle}>Profile</a>
-        </Link>
-        {
-          authenticatedState === 'not-authenticated' || (
-            <Link href='/login'>
-              <a style={linkStyle}>Sign In</a>
-            </Link>
-          )
-        }
-        <Link href='/protected'>
-        <a style={linkStyle}>Protected</a>
-        </Link>
-      </nav> */}
       <Component {...pageProps} />
-    </div>
   )
-}
-
-const linkStyle = {
-  marginRight: 10
-}
-
-const navStyle = {
-  margin: 20
 }
 
 export default MyApp
--- pages/api/auth.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from "../../util/supabaseClient";
-
-const handler = async (req, res) => {
-    await supabase.auth.api.setAuthCookie(req, res);
-  };
-  
-export default handler;
